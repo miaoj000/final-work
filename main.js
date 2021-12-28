6ed82f1092677e3120eada53106507ae89400253
miaoj000 <1712446101@qq.com>
2021-12-28 11:29:12 +0800

    finished waitingforchangedatabaseandupload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const e = require('express')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 const port = 10520
-mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://172.21.2.236/test');数据库请链接你的数据库
 const userinfo = mongoose.model('user', { 'name': String, 'phoneNumber': String, 'username': { type: String, unique: true }, 'password': String, 'administrator': { type: Boolean, default: false } })
 const household = mongoose.model('household', { 'name': String, 'location': Number, 'houseNumber': Number })
 const requestchange = mongoose.model('request', { 'name': String, 'phoneNumber': String, 'location': Number, 'houseNumber': Number, 'status': String })
@@ -52,27 +52,26 @@ app.use('/login', (req, res) => {
             var numbers_fit = counts
             userinfo.findOne({ 'username': username, 'password': password }, callback = (err, infos) => {
                 if (err) console.log(err)
-                if (numbers_fit) {
-                    res.status(200)
-                    res.set('Content-Type', 'text/html')
-                    req.session.username = username
-                    req.session.administrator = infos.administrator
-                    req.session.password = password
-                    req.session.name = infos.name
-                    req.session.number = infos.phoneNumber
-                    fs.readFile('./WebContent/loginsuc.html', 'utf-8', (err, data) => {
-                        if (err) {
-                            res.send("<p>login error!</p>")
-                        } else {
-                            res.send(data)
-                        }
-                        res.end()
-                    })
-                } else {
-                    ejs.renderFile('./WebContent/login.html', data = { tip: "用户名或密码错误！" }, (err, str) => {
-                        res.send(str)
-                    })
-                }
+                else{
+                    if (numbers_fit) {
+                        req.session.username = username
+                        req.session.administrator = infos.administrator
+                        req.session.password = password
+                        req.session.name = infos.name
+                        req.session.number = infos.phoneNumber
+                        fs.readFile('./WebContent/loginsuc.html', 'utf-8', (err, data) => {
+                            if (err) {
+                                res.send("<p>login error!</p>")
+                            } else {
+                                res.send(data)
+                            }
+                        })
+                    } else {
+                        ejs.renderFile('./WebContent/login.html', data = { tip: "用户名或密码错误！" }, (err, str) => {
+                            res.send(str)
+                        })
+                    }
+                } 
             })
         }
     })
@@ -80,7 +79,7 @@ app.use('/login', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.sendFile('add.html', { root: path.join(__dirname, 'WebContent') }, (err) => {
-        console.log(err)
+        res.send("操作失败！")
     })
 })
 
@@ -98,7 +97,7 @@ app.post('/add', (req, res) => {
             var housePer = new userinfo({ 'name': name, 'phoneNumber': number, 'username': username, 'password': password })
             housePer.save()
             res.sendFile('regcomplete.html', { root: path.join(__dirname, 'WebContent') }, (err) => {
-                console.log(err)
+                res.send("操作失败！")
             })
         }
     })
@@ -125,7 +124,7 @@ app.use('/info', (req, res) => {
                         requestchange.count({ 'name': req.session.name }, (err, counts) => {
                             if (counts) {
                                 requestchange.find({ 'name': req.session.name }, 'location houseNumber', (err, results) => {
-                                    var str
+                                    var str = ""
                                     for (i = 0; i < counts; i++) {
                                         if (results[i].status == 'add') {
                                             str = str + "申请添加:" + results[i].location + '幢' + results[i].houseNumber + '室' + '\n'
@@ -170,13 +169,14 @@ app.get('/list', (req, res) => {
     var htmlRe = ""
     requestchange.count((err, counts) => {
         if (counts) {
-            htmlRe = htmlRe + "<table class='table col-sm-12'><thead><tr><th scope='col'>申请者</th><th scope='col'>联系方式</th><th scope='col'>申请住宅</th><th scope='col'>申请内容</th><th scope='col'>处理方式</th><th scope='col'>处理方式</th></tr></thead><tbody>"
+            htmlRe = htmlRe + "<table class='table col-sm-12'><thead><tr><th scope='col'>申请者</th><th scope='col'>联系方式</th><th scope='col'>申请楼号</th><th scope='col'>申请户号</th><th scope='col'>申请内容</th><th scope='col'>处理方式</th><th scope='col'>处理方式</th></tr></thead><tbody>"
             requestchange.find({}, null, { limit: 5 }, (err, result) => {
                 result.forEach((value, index) => {
                     htmlRe = htmlRe + "<tr>"
                     htmlRe = htmlRe + "<td height=10% id='name"+index+"'>"+value.name+"</td>"
                     htmlRe = htmlRe + "<td id='number"+index+"'>"+value.phoneNumber+"</td>"
-                    htmlRe = htmlRe + "<td id='location"+index+"'>"+value.location+"幢"+value.houseNumber+"室</td>"
+                    htmlRe = htmlRe + "<td id='lou"+index+"'>"+value.location+"</td>"
+                    htmlRe = htmlRe + "<td id='hu"+index+"'>"+value.houseNumber+"</td>"
                     htmlRe = htmlRe + "<td id='statu"+index+"'>"+value.status+"</td>"
                     htmlRe = htmlRe + "<td id='acc"+index+"'><button class='btn btn-primary' onclick = 'acc("+index+")'>同意</td>"
                     htmlRe = htmlRe + "<td id='acc"+index+"'><button class='btn btn-danger' onclick = 'rej("+index+")'>拒绝</td>"
@@ -193,6 +193,49 @@ app.get('/list', (req, res) => {
 app.get('/change', (req, res) => {
     res.sendFile('change.html', { root: path.join(__dirname, 'WebContent') }, (err) => {
         console.log(err)
+    })
+})
+
+app.post('/accept',(req,res)=>{
+    if (req.body.statu === '添加'){
+        var newHouse = new household({'name':req.body.name,'location':parseInt(req.body.lou),'houseNumber':parseInt(req.body.hu)})
+        newHouse.save().then((value)=>{
+            requestchange.deleteOne({'name':req.body.name,'location':parseInt(req.body.lou),'houseNumber':parseInt(req.body.hu)},(err)=>{
+                if(err) {
+                    console.log(err)
+                }
+                else{
+                    res.send("操作成功！")
+                }
+            })
+        })
+    } else if(req.body.statu === '删除'){
+        household.deleteOne({'name':req.body.name,'location':parseInt(req.body.lou),'houseNumber':parseInt(req.body.hu)},(err)=>{
+            if(err)console.log(err)
+            else{
+                requestchange.deleteOne({'name':req.body.name,'location':parseInt(req.body.lou),'houseNumber':parseInt(req.body.hu)},(err)=>{
+                    if(err) {
+                        console.log(err)
+                    }
+                    else{
+                        res.send("操作成功！")
+                    }
+                })
+            }
+        })
+    }else{
+        res.send("状态错误，请检查数据库与前端页面！")
+    }
+})
+
+app.post('/reject',(req,res)=>{
+    requestchange.deleteOne({'name':req.body.name,'location':parseInt(req.body.lou),'houseNumber':parseInt(req.body.hu)},(err)=>{
+        if(err) {
+            console.log(err)
+        }
+        else{
+            res.send("操作成功！")
+        }
     })
 })
 
@@ -223,8 +266,6 @@ app.post('/rmChange', (req, res) => {
     var hounum = req.body.hu
     household.count({ 'location': loc, 'houseNumber': hounum, 'name': { '$ne': req.session.name } }, (err, counts1) => {
         requestchange.count({ 'location': loc, 'houseNumber': hounum }, (err, counts2) => {
-            console.log(counts1)
-            console.log(counts2)
             if (counts1 + counts2) {
                 res.send("该房屋已被注册或申请中。")
             } else {
